76bdc4f2b882ac260287371705f57663c0478ecb
28kaya <28kayak@gmail.com>
2017-02-10 01:14:19 -0800

    init

---
+++ app/src/main/java/com/example/kaya/text1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kaya.text1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        //method that will activated when an activity is loaded
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
